9cfbbdea25a820d3e98e5b6c111de60f9ea063a3
louyilin <louyilin1996@gmail.com>
2020-06-11 19:44:26 -0400

    6.11

--- src/main/java/com/lyl/sprintbootbooklist/web/BookController.java
+++ src/main/java/com/lyl/sprintbootbooklist/web/BookController.java
@@ -8,6 +8,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.List;
+
 /**
  * @author Yilinlou
  * @date 5/31/20 7:46 下午
@@ -18,9 +20,17 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class BookController {
     @Autowired
     private BookService bookService;
-
+/**
+* @Description: 获取全部书单列表
+* @Param: [model]
+* @return: java.lang.String
+* @Author: Yilin Lou
+* @Date: 6/11/20
+*/
     @GetMapping("/books")
-    public String list(){
+    public String list(Model model){
+        List<Book> books=bookService.findAll();
+        model.addAttribute("books",books);
         return "books";     //返回对应模版名字，HTML
     }
     @GetMapping("/books/{id}")
